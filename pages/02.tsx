579ced8052f3c63b167d8e270a86b0d6f4acdfdb
刘浩奇 <liuhaoqi@yaozai.net>
2023-01-10 17:54:45 +0800

    feat: modal

---
+++ pages/02.tsx
@@ -0,0 +1,204 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div className=" flex items-center justify-center min-h-screen bg-slate-800">
+      <div className=" flex flex-col my-6 space-y-6 md:space-y-0 md:space-x-6 md:flex-row md:my-0">
+        <div className=" bg-slate-700 rounded-xl text-white">
+          <div className=" p-8 mx-3 mt-3 rounded-t-xl bg-slate-800">
+            <div className=" text-center uppercase">基础版</div>
+            <h2 className=" mt-10 font-serif text-5xl text-center">100GB</h2>
+            <h3 className=" mt-2 text-center">$1.99</h3>
+            <div className=" flex justify-center">
+              <a
+                href="#"
+                className=" inline-block px-10 py-3 my-6 text-center border border-violet-600 rounded-lg duration-200 hover:bg-violet-800 hover:border-violet-800">
+                购买
+              </a>
+            </div>
+          </div>
+          <div className=" border-t border-slate-700"></div>
+          <div className=" p-8 mx-3 mb-3 rounded-b-xl bg-slate-800">
+            <div className=" flex flex-col space-y-2">
+              <div className=" flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <span className=" text-sm ml-1">100GB存储空间</span>
+              </div>
+              <div className=" flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <span className=" text-sm ml-1">加入会员</span>
+              </div>
+              <div className=" flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <span className=" text-sm ml-1">24/7技术支持</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className=" bg-violet-600 rounded-xl text-white">
+          <div className=" p-8 mx-3 mt-3 rounded-t-xl bg-slate-800">
+            <div className=" text-center uppercase">基础版</div>
+            <h2 className=" mt-10 font-serif text-5xl text-center">100GB</h2>
+            <h3 className=" mt-2 text-center">$1.99</h3>
+            <div className=" flex justify-center">
+              <a
+                href="#"
+                className=" inline-block bg-violet-600 px-10 py-3 my-6 text-center border border-violet-600 rounded-lg duration-200 hover:bg-violet-800 hover:border-violet-800">
+                购买
+              </a>
+            </div>
+          </div>
+          <div className=" border-t border-slate-700"></div>
+          <div className=" p-8 mx-3 mb-3 rounded-b-xl bg-slate-800">
+            <div className=" flex flex-col space-y-2">
+              <div className=" flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <span className=" text-sm ml-1">100GB存储空间</span>
+              </div>
+              <div className=" flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <span className=" text-sm ml-1">加入会员</span>
+              </div>
+              <div className=" flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <span className=" text-sm ml-1">24/7技术支持</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className=" bg-slate-700 rounded-xl text-white">
+          <div className=" p-8 mx-3 mt-3 rounded-t-xl bg-slate-800">
+            <div className=" text-center uppercase">基础版</div>
+            <h2 className=" mt-10 font-serif text-5xl text-center">100GB</h2>
+            <h3 className=" mt-2 text-center">$1.99</h3>
+            <div className=" flex justify-center">
+              <a
+                href="#"
+                className=" inline-block px-10 py-3 my-6 text-center border border-violet-600 rounded-lg duration-200 hover:bg-violet-800 hover:border-violet-800">
+                购买
+              </a>
+            </div>
+          </div>
+          <div className=" border-t border-slate-700"></div>
+          <div className=" p-8 mx-3 mb-3 rounded-b-xl bg-slate-800">
+            <div className=" flex flex-col space-y-2">
+              <div className=" flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <span className=" text-sm ml-1">100GB存储空间</span>
+              </div>
+              <div className=" flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <span className=" text-sm ml-1">加入会员</span>
+              </div>
+              <div className=" flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                <span className=" text-sm ml-1">24/7技术支持</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Cards
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
 import type { NextPage } from 'next'
-
+import Link from 'next/link'
 
 const Home: NextPage = () => {
-  return <div></div>
+  return (
+    <div>
+      <div className=" flex  flex-col gap-4">
+        <Link href={'/01'}>01</Link>
+        <Link href={'/02'}>02</Link>
+        <Link href={'/03'}>03</Link>
+        <Link href={'/04'}>04</Link>
+        <Link href={'/05'}>05</Link>
+        <Link href={'/06'}>06</Link>
+      </div>
+    </div>
+  )
 }
 
 export default Home
