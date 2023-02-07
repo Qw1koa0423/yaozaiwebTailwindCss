fcd43a7eeb3cb0bc3c8d43394ddec26a7556f461
刘浩奇 <liuhaoqi@yaozai.net>
2023-02-07 16:13:30 +0800

    ✨ feat(paas): 新增页
    前三屏

---
+++ constants/index.tsx
@@ -0,0 +1,24 @@
+export const PASS_DATA_TWO: { img: string; title: string; content: string }[] =
+  [
+    {
+      img: '/paas/cdyc.png',
+      title: '超低延迟',
+      content:
+        '端到端延时＜300ms，适用于多人实时互动场景，体验面对面聊天的沉浸感',
+    },
+    {
+      img: '/paas/cgqtx.png',
+      title: '超高清图像',
+      content: '优异的画质表现，12k分辨率超采拼接，真8k(7680*4320)分辨率显示',
+    },
+    {
+      img: '/paas/gjbaqfh.png',
+      title: '高级别安全防护',
+      content: '动态交换密钥，视频数据全程加密，保证企业场景安全性',
+    },
+    {
+      img: '/paas/gyzcjcs.png',
+      title: '高音质采集传输',
+      content: '支持麦克风阵列、高采样，全链路192kbps编码传输音频，听感清晰',
+    },
+  ]
